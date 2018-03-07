6da941e808a2e1e858e1c20ecf0df70b3bd3d932
wongwill86-mv-2 <wongwill86@gmail.com>
2018-03-06 19:14:47 -0500

    Share default Resources object when no Resources specified
    This has 4 calls to configuration.getInt which is terribly slow.
    WARNING: Tasks are now longer allowed to change its cpu/ram/disk/gpu
    resources after it has been created. The default Resources object will
    be shared by all tasks if none is specified

--- airflow/models.py
+++ airflow/models.py
@@ -2237,7 +2237,7 @@ class BaseOperator(LoggingMixin):
             on_success_callback=None,
             on_retry_callback=None,
             trigger_rule=TriggerRule.ALL_SUCCESS,
-            resources=None,
+            resources=Resources(),
             run_as_user=None,
             task_concurrency=None,
             *args,
@@ -2311,7 +2311,11 @@ class BaseOperator(LoggingMixin):
                         d=dag.dag_id if dag else "", t=task_id, tr=weight_rule))
         self.weight_rule = weight_rule
 
-        self.resources = Resources(**(resources or {}))
+        if isinstance(resources, Resources):
+            self.resources = resources
+        else:
+            self.resources = Resources(**(resources))
+
         self.run_as_user = run_as_user
         self.task_concurrency = task_concurrency
 
